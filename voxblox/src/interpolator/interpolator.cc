e5ac22e444a05a78e234e2e312e29dc91e91678f
Johannes Pankert <Johannes.Pankert@mavt.ethz.ch>
2019-12-19 13:24:09 +0100

    specializations for caching esdf interpolation functions

---
+++ voxblox/src/interpolator/interpolator.cc
@@ -0,0 +1,80 @@
+//
+// Created by johannes on 18.12.19.
+//
+#include <voxblox/interpolator/interpolator.h>
+
+using namespace voxblox;
+
+template <>
+bool Interpolator<EsdfCachingVoxel>::getInterpolatedDistanceGradientHessian(
+    const Point& pos, FloatingPoint* distance, Point* gradient,
+    Eigen::Matrix<FloatingPoint, 3, 3>* hessian) const {
+  typename Layer<EsdfCachingVoxel>::BlockType::ConstPtr block_ptr =
+      layer_->getBlockPtrByCoordinates(pos);
+  if (block_ptr == nullptr) {
+    return false;
+  }
+  VoxelIndex voxel_index =
+      block_ptr->computeTruncatedVoxelIndexFromCoordinates(pos);
+  Point voxel_pos = block_ptr->computeCoordinatesFromVoxelIndex(voxel_index);
+
+  const EsdfCachingVoxel* voxel_ptr = block_ptr->getVoxelPtrByCoordinates(pos);
+
+  if (voxel_ptr) {
+    float_t voxelSizeInv = block_ptr->voxel_size_inv();
+    Point offset = (pos - voxel_pos);
+    *distance = voxel_ptr->distance + voxel_ptr->gradient.dot(offset) +
+                0.5 * offset.dot(voxel_ptr->hessian * offset);
+    *gradient = voxel_ptr->gradient + voxel_ptr->hessian * offset;
+    *hessian = voxel_ptr->hessian;
+    return true;
+  }
+  return false;
+}
+
+template <>
+bool Interpolator<EsdfCachingVoxel>::getInterpolatedGradient(
+    const Point& pos, Point* grad) const {
+  typename Layer<EsdfCachingVoxel>::BlockType::ConstPtr block_ptr =
+      layer_->getBlockPtrByCoordinates(pos);
+  if (block_ptr == nullptr) {
+    return false;
+  }
+  VoxelIndex voxel_index =
+      block_ptr->computeTruncatedVoxelIndexFromCoordinates(pos);
+  Point voxel_pos = block_ptr->computeCoordinatesFromVoxelIndex(voxel_index);
+
+  const EsdfCachingVoxel* voxel_ptr = block_ptr->getVoxelPtrByCoordinates(pos);
+
+  if (voxel_ptr) {
+    float_t voxelSizeInv = block_ptr->voxel_size_inv();
+    Point offset = (pos - voxel_pos);
+    *grad = voxel_ptr->gradient + voxel_ptr->hessian * offset;
+    return true;
+  }
+  return false;
+}
+
+template <>
+bool Interpolator<EsdfCachingVoxel>::getInterpolatedDistance(
+    const Point& pos, FloatingPoint* distance) const {
+  typename Layer<EsdfCachingVoxel>::BlockType::ConstPtr block_ptr =
+      layer_->getBlockPtrByCoordinates(pos);
+  if (block_ptr == nullptr) {
+    return false;
+  }
+  VoxelIndex voxel_index =
+      block_ptr->computeTruncatedVoxelIndexFromCoordinates(pos);
+  Point voxel_pos = block_ptr->computeCoordinatesFromVoxelIndex(voxel_index);
+
+  const EsdfCachingVoxel* voxel_ptr = block_ptr->getVoxelPtrByCoordinates(pos);
+
+  if (voxel_ptr) {
+    float_t voxelSizeInv = block_ptr->voxel_size_inv();
+    Point offset = (pos - voxel_pos);
+    *distance = voxel_ptr->distance + voxel_ptr->gradient.dot(offset) +
+                0.5 * offset.dot(voxel_ptr->hessian * offset);
+    return true;
+  }
+  return false;
+}
